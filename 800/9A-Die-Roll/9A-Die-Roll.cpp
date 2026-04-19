c1eb7ee1d4dac1d777a3f2fed6bb1009dc801376
rif-x43 <rif-x43@users.noreply.github.com>
2026-04-20 01:03:17 +0600

    [9A-Die-Roll| 800] Accepted | Time:92ms | Memory:0KB

---
+++ 800/9A-Die-Roll/9A-Die-Roll.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+// reusRIFX---
+using namespace std;
+
+
+#define int long long
+#define endl "\n"
+
+#define all(v) v.begin(), v.end()
+#define rall(v) v.rbegin(), v.rend() // For sorting in descending order 
+
+const int INF = 1e18; // Large value for "infinity" 
+
+int lcm(int a, int b) {
+    return a / gcd(a,b) * b;
+} 
+
+
+void solve() {
+    int x, y;
+    cin >> x >> y;
+
+    int A = 7 - max(x,y), B = 6;
+    cout << A / gcd(A,6) << "/" << B / gcd(A,6) << endl;
+}
+
+int32_t main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t = 1;
+    //cin >> t;
+
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+}
