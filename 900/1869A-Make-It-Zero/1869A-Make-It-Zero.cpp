391405d55c59003002a9f3ba3837e470d5c1c6c0
rif-x43 <rif-x43@users.noreply.github.com>
2026-04-20 02:43:56 +0600

    [1869A-Make-It-Zero| 900] Accepted | Time:31ms | Memory:0KB

---
+++ 900/1869A-Make-It-Zero/1869A-Make-It-Zero.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+ // reusRIFX---
+using namespace std;
+
+
+#define int long long
+#define endl "\n"
+
+#define all(v) v.begin(), v.end()
+#define rall(v) v.rbegin(), v.rend() // For sorting in descending order 
+
+const int INF = 1e18; // Large value for "infinity" 
+
+int lcm(int a, int b) {
+    return a / gcd(a, b) * b;
+}
+
+
+void solve() {
+    int n;
+    cin >> n;
+    vector < int > a(n);
+    for (auto & x: a) cin >> x;
+    if (n & 1) {
+        cout << 4 << endl;
+        cout << 1 << " " << n - 1 << endl;
+        cout << 1 << " " << n - 1 << endl;
+        cout << n - 1 << " " << n << endl;
+        cout << n - 1 << " " << n << endl;
+    } else {
+        cout << 2 << endl;
+        cout << 1 << " " << n << endl;
+        cout << 1 << " " << n << endl;
+
+    }
+}
+
+int32_t main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t = 1;
+    cin >> t;
+
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+}
