ad39f9044a75e6ca6e9a051aad5fd938203b0577
rif-x43 <rif-x43@users.noreply.github.com>
2026-04-30 20:50:22 +0600

    [2227B-Party-Monster| unrated] Accepted | Time:31ms | Memory:0KB

---
+++ Unrated/2227B-Party-Monster/2227B-Party-Monster.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+// reusRIFX---
+using namespace std;
+
+
+#define int long long
+#define endl "\n"
+
+#define all(v) v.begin(), v.end()
+#define rall(v) v.rbegin(), v.rend() // For sorting in descending order 
+
+const int INF = 1e18; // Large value for "infinity" 
+
+int lcm(int a, int b) {
+    return a / gcd(a,b) * b;
+} 
+
+
+void solve() {
+    int n;
+    cin >> n;
+    string s;
+    cin >> s;
+    
+    int countLeft = 0, countRight = 0;
+    for (int i = 0; i < n; i++) {
+        if (s[i] == '(') countLeft++;
+        else countRight++;
+    }
+    
+    cout << (countLeft == countRight ? "YES" : "NO") << endl;
+    
+}
+
+int32_t main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t = 1;
+    cin >> t;
+
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+}
