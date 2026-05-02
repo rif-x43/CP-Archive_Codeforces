55404169aea7845da819c2fb5370d4ede3044b4b
rif-x43 <rif-x43@users.noreply.github.com>
2026-05-02 16:24:12 +0600

    [1722A-Spell-Check| 800] Accepted | Time:15ms | Memory:0KB

---
+++ 800/1722A-Spell-Check/1722A-Spell-Check.cpp
@@ -0,0 +1,58 @@
+#include <bits/stdc++.h>
+ // reusRIFX---
+using namespace std;
+
+
+#define int long long
+#define endl "\n"
+
+#define all(v) v.begin(), v.end()
+#define rall(v) v.rbegin(), v.rend() // For sorting in descending order 
+
+#define Y "YES"
+#define N "NO"
+
+const int INF = 1e18; // Large value for "infinity" 
+const int MAXN = 200005;
+const int MOD = 676767677;
+
+int lcm(int a, int b) {
+    return a / gcd(a, b) * b;
+}
+
+
+void solve() {
+    int n;
+    cin >> n;
+    string s;
+    cin >> s;
+
+    string t = "Timur";
+    bool isPossible = true;
+    if (n != 5) isPossible = false;
+    else {
+        for (int i = 0; i < 5; i++) {
+            size_t idx = s.find(t[i]);
+            if (idx == string::npos) {
+                isPossible = false;
+                break;
+            }
+        }
+    }
+    
+    cout << (isPossible ? Y : N) << endl;
+}
+
+int32_t main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t = 1;
+    cin >> t;
+
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+}
