79a3ab06aa52b0c920a3d7e0f094dbca3996fc92
rif-x43 <rif-x43@users.noreply.github.com>
2026-05-07 22:57:46 +0600

    [677A - Vanya and Fence | 800] Accepted | Time: 31ms | Memory: 100KB

---
+++ 800/Div. 2/677A - Vanya and Fence/677A - Vanya and Fence.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+// reusRIFX---
+using namespace std;
+
+
+#define int long long
+#define endl "\n"
+
+#define all(v) v.begin(), v.end()
+#define rall(v) v.rbegin(), v.rend() // For sorting in descending order 
+
+const int INF = 1e18; // Large value for "infinity" 
+
+int lcm(int a, int b) {
+    return a / gcd(a,b) * b;
+} 
+
+
+void solve() {
+    int n,h;
+    cin >> n >> h;
+    int width = 0;
+    for (int i = 0; i < n; i++){
+        int x;
+        cin >> x;
+        
+        width++;
+        if(x > h) width++;
+    }
+    
+    cout << width << endl;
+}
+
+int32_t main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t = 1;
+    //cin >> t;
+
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+}
