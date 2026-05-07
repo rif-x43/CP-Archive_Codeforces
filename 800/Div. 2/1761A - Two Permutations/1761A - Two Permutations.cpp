5d20aa966db76bf26d817a3b861f625e58ad1544
rif-x43 <rif-x43@users.noreply.github.com>
2026-05-07 23:12:19 +0600

    [1761A - Two Permutations | 800] Accepted | Time: 31ms | Memory: 0KB

---
+++ 800/Div. 2/1761A - Two Permutations/1761A - Two Permutations.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+ // reusRIFX---
+using namespace std;
+
+
+#define int long long
+#define endl "\n"
+
+#define all(v) v.begin(), v.end()
+#define rall(v) v.rbegin(), v.rend() // For sorting in descending order 
+
+const int INF = 1e18; // Large value for "infinity" 
+
+int lcm(int a, int b) {
+    return a / gcd(a, b) * b;
+}
+
+
+void solve() {
+    int n, a, b;
+    cin >> n >> a >> b;
+    
+    bool isPossible = (a + b <= n - 2) || (a == n && b == n) ;
+    
+    cout << (isPossible ? "YES" : "NO") << endl;
+}
+
+int32_t main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t = 1;
+    cin >> t;
+
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+}
