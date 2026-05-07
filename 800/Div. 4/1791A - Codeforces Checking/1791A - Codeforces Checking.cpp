fc7d490206901d9a5c79a114fb734037c6adc738
rif-x43 <rif-x43@users.noreply.github.com>
2026-05-07 22:57:02 +0600

    [1791A - Codeforces Checking | 800] Accepted | Time: 15ms | Memory: 0KB

---
+++ 800/Div. 4/1791A - Codeforces Checking/1791A - Codeforces Checking.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+ // reusRIFX---
+using namespace std;
+
+
+#define int long long
+#define endl "\n"
+
+#define all(v) v.begin(), v.end()
+#define rall(v) v.rbegin(), v.rend() // For sorting in descending order 
+
+const int INF = 1e18; // Large value for "infinity" 
+
+int lcm(int a, int b) {
+    return a / gcd(a, b) * b;
+}
+
+
+void solve() {
+    char c;
+    cin >> c;
+
+    string cf = "codeforces";
+    size_t idx = cf.find(c);
+    cout << (idx != string::npos ? "YES" : "NO") << endl;
+}
+
+int32_t main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t = 1;
+    cin >> t;
+
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+}
