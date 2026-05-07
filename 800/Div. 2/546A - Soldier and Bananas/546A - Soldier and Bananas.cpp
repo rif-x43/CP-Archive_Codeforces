0dd98e4b57e012e2856ac8f9729a87b269cc0858
rif-x43 <rif-x43@users.noreply.github.com>
2026-05-07 23:07:35 +0600

    [546A - Soldier and Bananas | 800] Accepted | Time: 46ms | Memory: 0KB

---
+++ 800/Div. 2/546A - Soldier and Bananas/546A - Soldier and Bananas.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+ // reusRIFX---
+using namespace std;
+
+
+#define int long long
+#define endl "\n"
+
+#define all(v) v.begin(), v.end()
+#define rall(v) v.rbegin(), v.rend() // For sorting in descending order 
+
+const int INF = 1e18; // Large value for "infinity" 
+
+int lcm(int a, int b) {
+    return a / gcd(a, b) * b;
+}
+
+
+void solve() {
+    int k, n, w;
+    cin >> k >> n >> w;
+    
+    int price = (((w * (w + 1)) / 2) * k);
+    int borrow = (price > n) ? (price - n) : 0;
+    
+    cout << borrow << endl;
+
+}
+
+int32_t main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t = 1;
+    //cin >> t;
+
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+}
