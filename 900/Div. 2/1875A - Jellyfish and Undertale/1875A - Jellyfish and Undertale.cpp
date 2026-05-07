2be590184cfb8f8980ca691db63cd17b8ec887f4
rif-x43 <rif-x43@users.noreply.github.com>
2026-05-07 22:59:34 +0600

    [1875A - Jellyfish and Undertale | 900] Accepted | Time: 46ms | Memory: 0KB

---
+++ 900/Div. 2/1875A - Jellyfish and Undertale/1875A - Jellyfish and Undertale.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+// reusRIFX---
+using namespace std;
+
+
+#define int long long
+#define endl "\n"
+
+#define all(v) v.begin(), v.end()
+#define rall(v) v.rbegin(), v.rend() // For sorting in descending order 
+
+const int INF = 1e18; // Large value for "infinity" 
+
+int lcm(int a, int b) {
+    return a / gcd(a,b) * b;
+} 
+
+
+void solve() {
+    int a, b, n;
+    cin >> a >> b >> n;
+    vector < int > v(n);
+    
+    int res = b;
+    for (auto &x : v) {
+        cin >> x;
+        res+= min(x, a - 1);
+    }
+    
+    cout << res << endl;
+    
+}
+
+int32_t main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t = 1;
+    cin >> t;
+
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+}
