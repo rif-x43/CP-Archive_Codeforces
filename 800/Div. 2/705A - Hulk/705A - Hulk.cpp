a2f9159fb8662e746ebc91bea615867709b36c2d
rif-x43 <rif-x43@users.noreply.github.com>
2026-05-07 23:08:08 +0600

    [705A - Hulk | 800] Accepted | Time: 31ms | Memory: 100KB

---
+++ 800/Div. 2/705A - Hulk/705A - Hulk.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+// reusRIFX---
+using namespace std;
+
+
+#define int long long
+#define endl "\n"
+
+#define all(v) v.begin(), v.end()
+#define rall(v) v.rbegin(), v.rend() // For sorting in descending order 
+
+const int INF = 1e18; // Large value for "infinity" 
+
+int lcm(int a, int b) {
+    return a / gcd(a,b) * b;
+} 
+
+
+void solve() {
+    int n;
+    cin >> n;
+    
+    string result = "I hate ";
+    
+    for (int i = 2; i <= n; i++) {
+        if(i & 1) result += "that I hate ";
+        else result += "that I love ";
+    }
+    
+    result += "it";
+    
+    cout << result << endl;
+}
+
+int32_t main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t = 1;
+    //cin >> t;
+
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+}
