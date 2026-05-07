d5a09f65e565ba88b6bd4f2e41487613efd68d44
rif-x43 <rif-x43@users.noreply.github.com>
2026-05-07 23:19:42 +0600

    [977A - Wrong Subtraction | 800] Accepted | Time: 46ms | Memory: 100KB

---
+++ 800/Div. 3/977A - Wrong Subtraction/977A - Wrong Subtraction.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+ // reusRIFX---
+using namespace std;
+
+
+#define int long long
+#define endl "\n"
+
+#define all(v) v.begin(), v.end()
+#define rall(v) v.rbegin(), v.rend() // For sorting in descending order 
+#define all(s) s.begin(), s.end()
+
+const int INF = 1e18; // Large value for "infinity" 
+
+
+void solve() {
+    int n, k;
+    cin >> n >> k;
+
+    for (int i = 0; i < k; i++) {
+        if (n % 10 == 0)
+            n /= 10;
+        else
+            n--;
+    }
+
+    cout << n << endl;
+
+}
+
+int32_t main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t = 1;
+    //cin >> t;
+
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+}
