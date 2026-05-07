6f0a6d242af4898468f55b2c96122ef1cc885f75
rif-x43 <rif-x43@users.noreply.github.com>
2026-05-07 23:21:20 +0600

    [2171A - Shizuku Hoshikawa and Farm Legs | 800] Accepted | Time: 31ms | Memory: 0KB

---
+++ 800/Div. 3/2171A - Shizuku Hoshikawa and Farm Legs/2171A - Shizuku Hoshikawa and Farm Legs.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+// reusRIFX---
+using namespace std;
+
+
+#define int long long
+#define endl "\n"
+
+#define all(v) v.begin(), v.end()
+#define rall(v) v.rbegin(), v.rend() // For sorting in descending order 
+
+const int INF = 1e18; // Large value for "infinity" 
+
+
+void solve() {
+    int n;
+    cin >> n;
+    
+    cout << ((n % 2 == 0) ? ((n / 4) + 1) : 0) << endl;
+    
+    
+}
+
+int32_t main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t = 1;
+    cin >> t;
+
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+}
