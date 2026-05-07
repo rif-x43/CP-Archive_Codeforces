2c13ca9976e8ecfa04f7bb94386d5ef6595f029b
rif-x43 <rif-x43@users.noreply.github.com>
2026-05-07 23:07:24 +0600

    [2211A - Antimedian Deletion | 800] Accepted | Time: 31ms | Memory: 0KB

---
+++ 800/Div. 2/2211A - Antimedian Deletion/2211A - Antimedian Deletion.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+// reusRIFX---
+using namespace std;
+
+
+#define int long long
+#define endl "\n"
+
+#define all(v) v.begin(), v.end()
+#define rall(v) v.rbegin(), v.rend() // For sorting in descending order 
+
+const int INF = 1e18; // Large value for "infinity" 
+
+int lcm(int a, int b) {
+    return a / gcd(a,b) * b;
+} 
+
+
+void solve() {
+    int n;
+    cin >> n;
+    vector < int > v(n);
+    for (auto &x : v) cin >> x;
+    
+    if (n == 1) cout << 1 << endl;
+    else {
+        for (int i = 0; i < n; i++) cout << 2 << " ";
+        cout << endl;
+    }
+    
+}
+
+int32_t main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t = 1;
+    cin >> t;
+
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+}
