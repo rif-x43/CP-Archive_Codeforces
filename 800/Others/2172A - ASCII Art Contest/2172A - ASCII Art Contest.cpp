2b8927e87de2eaad1b9de436b8c40573dcba51e8
rif-x43 <rif-x43@users.noreply.github.com>
2026-05-07 23:09:46 +0600

    [2172A - ASCII Art Contest | 800] Accepted | Time: 31ms | Memory: 0KB

---
+++ 800/Others/2172A - ASCII Art Contest/2172A - ASCII Art Contest.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+ // reusRIFX---
+using namespace std;
+
+
+#define int long long
+#define endl "\n"
+
+#define all(v) v.begin(), v.end()
+#define rall(v) v.rbegin(), v.rend() // For sorting in descending order 
+
+const int INF = 1e18; // Large value for "infinity" 
+
+int lcm(int a, int b) {
+    return a / gcd(a, b) * b;
+}
+
+
+void solve() {
+    vector < int > v(3);
+    cin >> v[0] >> v[1] >> v[2];
+
+    sort(all(v));
+    if (v[2] - v[0] >= 10) cout << "check again" << endl;
+    else cout << "final " << v[1] << endl;
+
+
+
+}
+
+int32_t main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t = 1;
+    //cin >> t;
+
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+}
