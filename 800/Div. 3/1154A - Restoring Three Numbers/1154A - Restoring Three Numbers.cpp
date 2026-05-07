a7ba4bd966898dd62ca920e4bf6b1b1196637df8
rif-x43 <rif-x43@users.noreply.github.com>
2026-05-07 23:16:55 +0600

    [1154A - Restoring Three Numbers | 800] Accepted | Time: 46ms | Memory: 0KB

---
+++ 800/Div. 3/1154A - Restoring Three Numbers/1154A - Restoring Three Numbers.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+// reusRIFX---
+using namespace std;
+
+
+#define int long long
+#define endl "\n"
+
+#define all(v) v.begin(), v.end()
+#define rall(v) v.rbegin(), v.rend() // For sorting in descending order 
+#define all(s) s.begin(), s.end()
+
+const int INF = 1e18; // Large value for "infinity" 
+
+
+void solve() {
+    vector <int> v(4);
+    for(int i = 0; i < 4; i++) cin >> v[i];
+    
+    sort(all(v));
+    
+    cout << v[3] - v[0] << " " << v[3] - v[1] << " " << v[3] - v[2] << endl;
+}
+
+int32_t main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t = 1;
+    //cin >> t;
+
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+}
