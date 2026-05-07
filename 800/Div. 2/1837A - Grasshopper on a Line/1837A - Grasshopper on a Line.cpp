5605cbb7f567f908ee543c875d7fc34f1ed2b27d
rif-x43 <rif-x43@users.noreply.github.com>
2026-05-07 23:15:50 +0600

    [1837A - Grasshopper on a Line | 800] Accepted | Time: 31ms | Memory: 100KB

---
+++ 800/Div. 2/1837A - Grasshopper on a Line/1837A - Grasshopper on a Line.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+// reusRIFX---
+using namespace std;
+
+
+#define int long long
+#define endl "\n"
+
+#define all(v) v.begin(), v.end()
+#define rall(v) v.rbegin(), v.rend() // For sorting in descending order 
+#define all(s) s.begin(), s.end()
+
+const int INF = 1e18; // Large value for "infinity" 
+
+
+void solve() {
+    int x, k;
+    cin >> x >> k;
+    
+    if(x % k == 0){
+        cout << 2 << endl;
+        cout << x - 1 << " " << 1 << endl;
+    }
+    else{
+        cout << 1 << endl;
+        cout << x << endl;
+    }
+}
+
+int32_t main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t = 1;
+    cin >> t;
+
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+}
