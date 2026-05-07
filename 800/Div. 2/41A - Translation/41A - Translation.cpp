f8e722d3b2e4caa980add6507c1eda7b4698ca6a
rif-x43 <rif-x43@users.noreply.github.com>
2026-05-07 23:05:06 +0600

    [41A - Translation | 800] Accepted | Time: 46ms | Memory: 0KB

---
+++ 800/Div. 2/41A - Translation/41A - Translation.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+// reusRIFX---
+using namespace std;
+
+
+#define int long long
+#define endl "\n"
+
+#define all(v) v.begin(), v.end()
+#define rall(v) v.rbegin(), v.rend() // For sorting in descending order 
+
+const int INF = 1e18; // Large value for "infinity" 
+
+int lcm(int a, int b) {
+    return a / gcd(a,b) * b;
+} 
+
+
+void solve() {
+    string s1, s2;
+    cin >> s1 >> s2;
+    
+    string srev(rall(s1));
+    cout << (srev == s2 ? "YES" : "NO" )<< endl;
+}
+
+int32_t main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t = 1;
+    //cin >> t;
+
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+}
