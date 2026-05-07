083ca3a510b9dfff44b5d32d96f7b2436a595b2f
rif-x43 <rif-x43@users.noreply.github.com>
2026-05-07 23:16:44 +0600

    [1878A - How Much Does Daytona Cost? | 800] Accepted | Time: 46ms | Memory: 100KB

---
+++ 800/Div. 3/1878A - How Much Does Daytona Cost/1878A - How Much Does Daytona Cost.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+// reusRIFX---
+using namespace std;
+
+
+#define int long long
+#define endl "\n"
+
+#define all(v) v.begin(), v.end()
+#define rall(v) v.rbegin(), v.rend() // For sorting in descending order 
+#define all(s) s.begin(), s.end()
+
+const int INF = 1e18; // Large value for "infinity" 
+
+
+void solve() {
+    int n, k;
+    cin >> n >> k;
+    
+    vector < int > v(n);
+    
+    for (int i = 0; i < n; i++) cin >> v[i];
+    
+    if(find(v.begin(), v.end(), k) != v.end()) cout << "YES" << endl;
+    else cout << "NO" << endl;
+}
+
+int32_t main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t = 1;
+    cin >> t;
+
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+}
