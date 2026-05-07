8ebbb467748ae5df447200ee9933f668b15e0de2
rif-x43 <rif-x43@users.noreply.github.com>
2026-05-07 23:00:06 +0600

    [2166A - Same Difference | 800] Accepted | Time: 46ms | Memory: 0KB

---
+++ 800/Div. 2/2166A - Same Difference/2166A - Same Difference.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+ // reusRIFX---
+using namespace std;
+
+
+#define int long long
+#define endl "\n"
+
+#define all(v) v.begin(), v.end()
+#define rall(v) v.rbegin(), v.rend() // For sorting in descending order 
+
+const int INF = 1e18; // Large value for "infinity" 
+
+int lcm(int a, int b) {
+    return a / gcd(a, b) * b;
+}
+
+
+void solve() {
+    int n;
+    string s;
+    cin >> n >> s;
+    int counter = 0;
+    for (int i = 0; i < n - 1; i++)
+        if (s[n - 1] != s[i]) counter++;
+
+    cout << counter << endl;
+
+}
+
+int32_t main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t = 1;
+    cin >> t;
+
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+}
