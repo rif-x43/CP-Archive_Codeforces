c17bfc52d91b4f4ae007e21ad21934620fa94004
rif-x43 <rif-x43@users.noreply.github.com>
2026-05-07 23:15:07 +0600

    [1814A - Coins | 800] Accepted | Time: 31ms | Memory: 100KB

---
+++ 800/Div. 2/1814A - Coins/1814A - Coins.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+ // reusRIFX---
+using namespace std;
+
+
+#define int long long
+#define endl "\n"
+
+#define all(v) v.begin(), v.end()
+#define rall(v) v.rbegin(), v.rend() // For sorting in descending order 
+#define all(s) s.begin(), s.end()
+
+const int INF = 1e18; // Large value for "infinity" 
+
+
+void solve() {
+    int n, k;
+    cin >> n >> k;
+
+    if (n < min(k, (int) 2)) cout << "NO" << endl;
+    else {
+        if (((n & 1) && (k & 1)) || !(n & 1)) cout << "YES" << endl;
+        else cout << "NO" << endl;
+    }
+}
+
+int32_t main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t = 1;
+    cin >> t;
+
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+}
