0ad2fdacb7d0ac64aec245bdf21dd02aa046bfe8
rif-x43 <rif-x43@users.noreply.github.com>
2026-05-07 22:59:12 +0600

    [2225A - A Number Between Two Others | unrated] Accepted | Time: 46ms | Memory: 100KB

---
+++ Unrated/Div. 2/2225A - A Number Between Two Others/2225A - A Number Between Two Others.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+// reusRIFX---
+using namespace std;
+
+
+#define int long long
+#define endl "\n"
+
+#define all(v) v.begin(), v.end()
+#define rall(v) v.rbegin(), v.rend() // For sorting in descending order 
+
+const int INF = 1e18; // Large value for "infinity" 
+
+int lcm(int a, int b) {
+    return a / gcd(a,b) * b;
+} 
+
+
+void solve() {
+    int x, y, z;
+    cin >> x >> y;
+    bool isPos;
+    if ((y - x) > x) isPos = true;
+    else isPos = false;
+    
+    cout << (isPos ? "YES" : "NO") << endl;
+}
+
+int32_t main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t = 1;
+    cin >> t;
+
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+}
