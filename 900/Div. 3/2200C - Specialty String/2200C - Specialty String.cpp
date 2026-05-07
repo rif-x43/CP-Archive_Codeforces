e68f35f6b14090782d23c7011947ee0d73b86aee
rif-x43 <rif-x43@users.noreply.github.com>
2026-05-07 23:22:44 +0600

    [2200C - Specialty String | 900] Accepted | Time: 46ms | Memory: 0KB

---
+++ 900/Div. 3/2200C - Specialty String/2200C - Specialty String.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+
+    while (t--) {
+        int n;
+        cin >> n;
+        string s;
+        cin >> s;
+
+        stack < char > st;
+        for (char c: s) {
+            if (!st.empty() && st.top() == c)
+                st.pop();
+            else
+                st.push(c);
+        }
+
+        cout << (st.empty() ? "YES" : "NO") << endl;
+    }
+}
