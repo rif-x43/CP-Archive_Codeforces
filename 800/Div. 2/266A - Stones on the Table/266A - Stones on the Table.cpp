d77b25a726eca21f212caa567b4b97ff72eafbd8
rif-x43 <rif-x43@users.noreply.github.com>
2026-05-07 23:10:13 +0600

    [266A - Stones on the Table | 800] Accepted | Time: 92ms | Memory: 100KB

---
+++ 800/Div. 2/266A - Stones on the Table/266A - Stones on the Table.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+ // reusRIFX---
+using namespace std;
+
+
+#define int long long
+#define endl "\n"
+
+#define all(v) v.begin(), v.end()
+#define rall(v) v.rbegin(), v.rend() // For sorting in descending order 
+
+const int INF = 1e18; // Large value for "infinity" 
+
+int lcm(int a, int b) {
+    return a / gcd(a, b) * b;
+}
+
+
+void solve() {
+    int n;
+    string s;
+    cin >> n >> s;
+
+    stack < char > st;
+    st.push(s[0]);
+    
+    for (int i = 1; i < n; i++)
+        if (s[i - 1] != s[i]) st.push(s[i]);
+
+    cout << n - st.size() << endl;
+}
+
+int32_t main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t = 1;
+    //cin >> t;
+
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+}
