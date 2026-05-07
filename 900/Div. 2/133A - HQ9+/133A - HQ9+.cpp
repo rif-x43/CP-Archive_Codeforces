fe7e2281f2434e152a18aef2f170887564b6c051
rif-x43 <rif-x43@users.noreply.github.com>
2026-05-07 23:07:56 +0600

    [133A - HQ9+ | 900] Accepted | Time: 92ms | Memory: 100KB

---
+++ 900/Div. 2/133A - HQ9+/133A - HQ9+.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+ // reusRIFX---
+using namespace std;
+
+
+#define int long long
+#define endl "\n"
+
+#define all(v) v.begin(), v.end()
+#define rall(v) v.rbegin(), v.rend() // For sorting in descending order 
+
+const int INF = 1e18; // Large value for "infinity" 
+
+int lcm(int a, int b) {
+    return a / gcd(a, b) * b;
+}
+
+
+void solve() {
+    string s;
+    cin >> s;
+    
+    string h = "HQ9";
+    bool isFound = false;
+    for (auto x : s) if (h.find(x) != string::npos) isFound = true; 
+    
+    cout << (isFound ? "YES" : "NO") << endl;
+    
+}
+
+int32_t main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t = 1;
+    //cin >> t;
+
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+}
