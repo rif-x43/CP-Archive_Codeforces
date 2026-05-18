beb7907d3a708d505399101a040dcd2f76810c05
rif-x43 <rif-x43@users.noreply.github.com>
2026-05-18 22:48:42 +0600

    [158B - Taxi | 1100] Accepted | Time: 124ms | Memory: 100KB

---
+++ 1100/Others/158B - Taxi/158B - Taxi.cpp
@@ -0,0 +1,61 @@
+#include <bits/stdc++.h>
+ // reusRIFX---
+using namespace std;
+
+
+#define int long long
+#define endl "\n"
+
+#define all(v) v.begin(), v.end()
+#define rall(v) v.rbegin(), v.rend() // For sorting in descending order 
+
+#define Y "YES"
+#define N "NO"
+
+const int INF = 1e18; // Large value for "infinity" 
+const int MAXN = 200005;
+const int MOD = 676767677;
+
+int lcm(int a, int b) {
+    return a / gcd(a, b) * b;
+}
+
+
+void solve() {
+    int n;
+    cin >> n;
+    vector<int> counts(5,0);
+    for (int i = 0; i < n; i++) {
+        int x;
+        cin >> x;
+        counts[x]++;
+    }
+
+    int cars = counts[4] + counts[3] + (counts[2] / 2);
+    counts[1] = max(0LL, counts[1] - counts[3]);
+    counts[2] %= 2;
+    
+    if (counts[2] > 0) {
+        cars++;
+        counts[1] = max(0LL, counts[1] - 2); 
+    }
+    
+    if (counts[1] > 0) cars += (counts[1] + 3) / 4;
+    
+    cout << cars << endl;
+    
+}
+
+int32_t main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t = 1;
+    //cin >> t;
+
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+}
