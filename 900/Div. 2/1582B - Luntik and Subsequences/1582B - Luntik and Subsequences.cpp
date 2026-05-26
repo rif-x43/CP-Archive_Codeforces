5a7442e0c7da8ac8ba3d1b0fad7b7575936e6221
rif-x43 <rif-x43@users.noreply.github.com>
2026-05-27 03:36:53 +0600

    [1582B - Luntik and Subsequences | 900] Accepted | Time: 46ms | Memory: 100KB

---
+++ 900/Div. 2/1582B - Luntik and Subsequences/1582B - Luntik and Subsequences.cpp
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+ // reusRIFX---
+using namespace std;
+
+
+#define int long long
+#define endl "\n"
+
+#define all(v) v.begin(), v.end()
+#define rall(v) v.rbegin(), v.rend() // For sorting in descending order 
+
+#define Y "YES"
+#define N "NO"
+
+const int INF = 1e18; // Large value for "infinity" 
+const int MAXN = 200005;
+const int MOD = 676767677;
+
+int lcm(int a, int b) {
+    return a / gcd(a, b) * b;
+}
+
+
+void solve() {
+    int n;
+    cin >> n;
+    
+    int ones = 0, zeros = 0;
+    for (int i = 0; i < n; i++) {
+        int x;
+        cin >> x;
+        if (x == 1) ones++;
+        else if (!x) zeros++;
+    }
+    
+    cout << (int) ones * (int) pow(2,zeros) << endl;
+}
+
+int32_t main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t = 1;
+    cin >> t;
+
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+}
