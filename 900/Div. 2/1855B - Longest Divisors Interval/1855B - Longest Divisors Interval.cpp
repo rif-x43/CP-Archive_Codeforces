d70ce18bf8bff79b050b4b6226eb2b26d37735da
rif-x43 <rif-x43@users.noreply.github.com>
2026-05-27 16:06:20 +0600

    [1855B - Longest Divisors Interval | 900] Accepted | Time: 46ms | Memory: 100KB

---
+++ 900/Div. 2/1855B - Longest Divisors Interval/1855B - Longest Divisors Interval.cpp
@@ -0,0 +1,63 @@
+#include <bits/stdc++.h>
+ // reusRIFX---
+using namespace std;
+
+
+#define int long long
+#define endl "\n"
+
+#define all(v) v.begin(), v.end()
+#define rall(v) v.rbegin(), v.rend() // For sorting in descending order 
+
+#define Y "YES"
+#define N "NO"
+
+const int INF = 1e18; // Large value for "infinity" 
+const int MAXN = 200005;
+const int MOD = 676767677;
+
+int lcm(int a, int b) {
+    return a / gcd(a, b) * b;
+}
+
+vector < int > get_divisors(int n) {
+    vector < int > res;
+    for (int i = 1; i * i <= n; ++i) {
+        if (n % i == 0) {
+            res.push_back(i);
+            if (i * i != n) res.push_back(n / i);
+        }
+    }
+    sort(all(res));
+    return res;
+}
+
+
+void solve() {
+    int n;
+    cin >> n;
+
+    vector < int > divisors;
+    
+    int cons = 0;
+    for (int i = 1; i <= n; i++) {
+        if (n % i == 0) cons++;
+        else break;
+    }
+    
+    cout << cons << endl;
+}
+
+int32_t main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t = 1;
+    cin >> t;
+
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+}
