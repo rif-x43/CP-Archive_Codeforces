fd5155cdb65373273ec8034a045379931e809a2e
rif-x43 <rif-x43@users.noreply.github.com>
2026-06-02 03:44:59 +0600

    [2156A - Pizza Time | 800] Accepted | Time: 46ms | Memory: 100KB

---
+++ 800/Div. 2/2156A - Pizza Time/2156A - Pizza Time.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+ // reusRIFX---
+using namespace std;
+ 
+ 
+#define int long long
+#define endl "\n"
+ 
+#define all(v) v.begin(), v.end()
+#define rall(v) v.rbegin(), v.rend() // For sorting in descending order 
+ 
+#define Y "YES"
+#define N "NO"
+ 
+const int INF = 1e18; // Large value for "infinity" 
+const int MAXN = 200005;
+const int MOD = 676767677;
+ 
+int lcm(int a, int b) {
+    return a / gcd(a, b) * b;
+}
+ 
+ 
+void solve() {
+    int n;
+    cin >> n;
+    int hao = 0;
+    while (n > 2) {
+        hao += n / 3;
+        n -= 2 * (n / 3);
+    }
+    cout << hao << endl;
+}
+ 
+int32_t main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+ 
+    int t = 1;
+    cin >> t;
+ 
+    while (t--) {
+        solve();
+    }
+ 
+    return 0;
+}
