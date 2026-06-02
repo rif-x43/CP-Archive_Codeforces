3e8397061f4c57b2f615140473e71d96279258d0
rif-x43 <rif-x43@users.noreply.github.com>
2026-06-03 02:00:53 +0600

    [102B - Sum of Digits | 1000] Accepted | Time: 92ms | Memory: 0KB

---
+++ 1000/Div. 2/102B - Sum of Digits/102B - Sum of Digits.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+// reusRIFX---
+using namespace std;
+
+
+#define int long long
+#define endl "\n"
+
+#define all(v) v.begin(), v.end()
+#define rall(v) v.rbegin(), v.rend() // For sorting in descending order 
+
+#define Y "YES"
+#define N "NO"
+
+const int INF = 1e18; // Large value for "infinity" 
+const int MAXN = 200005;
+const int MOD = 676767677;
+
+int lcm(int a, int b) {
+    return a / gcd(a,b) * b;
+} 
+
+
+void solve() {
+    string s; cin >> s;
+    
+    int count = 0;
+    while(s.size() > 1){
+        count++;
+        int sum = 0;
+        for(int i = 0; i < s.size(); i++) sum += s[i] - '0';
+        s = to_string(sum);
+    }
+    
+    cout << count << endl;
+}
+
+int32_t main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t = 1;
+    //cin >> t;
+
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+}
