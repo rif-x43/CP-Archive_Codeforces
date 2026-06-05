abf0b3cc29e7d2ea31d1c84bf4d8851286f7d4ac
rif-x43 <rif-x43@users.noreply.github.com>
2026-06-06 04:32:52 +0600

    [2205A - Simons and Making It Beautiful | 800] Accepted | Time: 46ms | Memory: 0KB

---
+++ 800/Div. 2/2205A - Simons and Making It Beautiful/2205A - Simons and Making It Beautiful.cpp
@@ -0,0 +1,54 @@
+#include <bits/stdc++.h>
+// reusRIFX---
+using namespace std;
+
+
+#define int long long
+#define endl "\n"
+
+#define all(v) v.begin(), v.end()
+#define rall(v) v.rbegin(), v.rend() // For sorting in descending order 
+
+#define Y "YES"
+#define N "NO"
+
+const int INF = 1e18; // Large value for "infinity" 
+const int MAXN = 200005;
+const int MOD = 676767677;
+
+int lcm(int a, int b) {
+    return a / gcd(a,b) * b;
+} 
+
+
+void solve() {
+    int n; cin >> n;
+    vector <int> a(n);
+    int mx = 0, idx = 0;
+    for (int i = 0; i < n; i++) {
+        cin >> a[i];
+        if (mx < a[i]) {
+            mx = a[i];
+            idx = i;
+        }
+    }
+    
+    swap(a[idx],a[0]);
+    
+    for(auto x : a) cout << x << " ";
+    cout << endl;
+}
+
+int32_t main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t = 1;
+    cin >> t;
+
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+}
