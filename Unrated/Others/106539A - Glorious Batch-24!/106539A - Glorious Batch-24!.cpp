8a81da7a4d23deb3b392263bcc9ec5d8264bd6fa
rif-x43 <rif-x43@users.noreply.github.com>
2026-06-08 01:23:00 +0600

    [106539A - Glorious Batch-24! | unrated] Accepted | Time: 62ms | Memory: 0KB

---
+++ Unrated/Others/106539A - Glorious Batch-24!/106539A - Glorious Batch-24!.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+ // reusRIFX---
+using namespace std;
+
+
+#define int long long
+#define endl "\n"
+
+#define all(v) v.begin(), v.end()
+#define rall(v) v.rbegin(), v.rend() // For sorting in descending order 
+
+#define Y "YES"
+#define N "NO"
+
+const int INF = 1e18; // Large value for "infinity" 
+const int MAXN = 200005;
+const int MOD = 676767677;
+
+int lcm(int a, int b) {
+    return a / gcd(a, b) * b;
+}
+
+
+void solve() {
+    int n;
+    cin >> n;
+    
+    int impostor = 0;
+    for (int i = 1; i <= n % 24 ; i++) {
+        if(((i * i) - 1) % 24 == 0) impostor++;
+    }
+    
+    cout << ((n / 24) * 8) + impostor << endl;
+}
+
+int32_t main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t = 1;
+    cin >> t;
+
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+}
