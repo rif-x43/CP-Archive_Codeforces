5b5668572c650e28b6a9e8c602b5421cab6677f0
rif-x43 <rif-x43@users.noreply.github.com>
2026-06-08 01:50:26 +0600

    [106539E - AutoCAD Mayhem | unrated] Accepted | Time: 31ms | Memory: 0KB

---
+++ Unrated/Others/106539E - AutoCAD Mayhem/106539E - AutoCAD Mayhem.cpp
@@ -0,0 +1,57 @@
+#include <bits/stdc++.h>
+ // reusRIFX---
+using namespace std;
+
+
+#define int long long
+#define endl "\n"
+
+#define all(v) v.begin(), v.end()
+#define rall(v) v.rbegin(), v.rend() // For sorting in descending order 
+
+#define Y "YES"
+#define N "NO"
+
+const int INF = 1e18; // Large value for "infinity" 
+const int MAXN = 200005;
+const int MOD = 676767677;
+
+int lcm(int a, int b) {
+    return a / gcd(a, b) * b;
+}
+
+
+void solve() {
+    int a = 0, b = 0;
+
+    for (int i = 0; i < 6; i++) {
+        int x;
+        cin >> x;
+        if (i < 3) a += x;
+        else b += x;
+    }
+    
+    bool flag = false;
+    if(a > b) {
+        if((a / b) * b == a) flag = true;
+    } else {
+        if((b / a) * a == b) flag = true;
+    }
+
+    cout << (flag ? Y : N) << endl;
+
+}
+
+int32_t main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t = 1;
+    //cin >> t;
+
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+}
