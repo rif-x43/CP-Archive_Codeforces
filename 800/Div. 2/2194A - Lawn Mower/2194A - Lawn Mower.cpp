d080abab3bf771c5b87e764295ad2b16a1b00963
rif-x43 <rif-x43@users.noreply.github.com>
2026-06-09 03:06:18 +0600

    [2194A - Lawn Mower | 800] Accepted | Time: 46ms | Memory: 0KB

---
+++ 800/Div. 2/2194A - Lawn Mower/2194A - Lawn Mower.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+ // reusRIFX---
+using namespace std;
+
+
+#define int long long
+#define endl "\n"
+
+#define all(v) v.begin(), v.end()
+#define rall(v) v.rbegin(), v.rend() // For sorting in descending order 
+
+#define Y "YES"
+#define N "NO"
+
+const int INF = 1e18; // Large value for "infinity" 
+const int MAXN = 200005;
+const int MOD = 676767677;
+
+int lcm(int a, int b) {
+    return a / gcd(a, b) * b;
+}
+
+
+void solve() {
+    int n, w;
+    cin >> n >> w;
+    if (w > n) cout << n << endl;
+    else if (w == 1) cout << 0 << endl;
+    else cout << ((n * (w - 1)) + (w - 1)) / w << endl;
+}
+
+int32_t main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t = 1;
+    cin >> t;
+
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+}
